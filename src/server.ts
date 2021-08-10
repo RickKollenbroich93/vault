e873734881011a9026b405af6524ecbf4a525e99
RickKollenbroich93 <rick.kollenbroich93@googlemail.com>
2021-08-10 16:02:39 +0200

    Add server express

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,16 @@
 import { readCredentials } from './utils/credentials';
 
-console.log('Hello server !!!!!!');
+// console.log('Hello server !!!!!!');
 
-readCredentials();
+// readCredentials();
+import express from 'express';
+const app = express();
+const port = 3000;
+
+app.get('/', (_req, res) => {
+  res.send('Hello World! Was geht ich bin dein SERVER!!!');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
