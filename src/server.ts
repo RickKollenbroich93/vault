353d88ae88a11e9e9c768cc932cdb030d1bab58b
RickKollenbroich93 <rick.kollenbroich93@googlemail.com>
2021-08-10 14:05:30 +0200

    Add db.json and Database related changes

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,11 @@
 console.log('Hello server !!!!!!');
+
+import { readFile } from 'fs/promises';
+
+async function readingPassword() {
+  const promise = await readFile('src/db.json', 'utf-8');
+
+  console.log(promise);
+  return promise;
+}
+readingPassword();
