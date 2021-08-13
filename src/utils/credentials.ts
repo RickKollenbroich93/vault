ed934100e8e089c48a67e57f66bd7612a8e27353
RickKollenbroich93 <rick.kollenbroich93@googlemail.com>
2021-08-13 14:08:38 +0200

    ♻️  Rename functions and remove unnecessary key argument

--- src/utils/credentials.ts
+++ src/utils/credentials.ts
@@ -1,21 +1,19 @@
-import { Credential, DB } from '../types';
+import type { Credential, DB } from '../types';
 import { readFile, writeFile } from 'fs/promises';
-import { deCrypt, enCript } from './crypto';
+import { decryptCredential, encryptCredential } from './crypto';
 
-export async function readCredentials(key: string): Promise<Credential[]> {
+export async function readCredentials(): Promise<Credential[]> {
   const response = await readFile('src/db.json', 'utf-8');
   const db: DB = JSON.parse(response);
   const credentials: Credential[] = db.credentials;
-  const deCryptCredentials = credentials.map((credential) =>
-    deCrypt(credential, key)
-  );
-  return deCryptCredentials;
+
+  return credentials;
 }
 export async function getCredential(
   service: string,
   key: string
 ): Promise<Credential> {
-  const credentials = await readCredentials(key);
+  const credentials = await readCredentials();
   const credential = credentials.find(
     (credential) => credential.service.toLowerCase() === service.toLowerCase()
   );
@@ -23,14 +21,11 @@ export async function getCredential(
   if (!credential) {
     throw new Error(`ERROR, NO such Service WuuuUHahaHA !!!: ${service}`);
   }
-  //   const deCryptCredential = deCrypt(credential, key);
-  return credential;
+  const decryptedCredential = decryptCredential(credential, key);
+  return decryptedCredential;
 }
-export async function deleteCredential(
-  service: string,
-  key: string
-): Promise<void> {
-  const credentials = await readCredentials(key);
+export async function deleteCredential(service: string): Promise<void> {
+  const credentials = await readCredentials();
   const filteredCredentials = credentials.filter(
     (credential) => credential.service.toLowerCase() !== service.toLowerCase()
   );
@@ -46,12 +41,12 @@ export async function updateCredential(
   credential: Credential,
   key: string
 ): Promise<void> {
-  const credentials = await readCredentials(key);
+  const credentials = await readCredentials();
   const filteredCredential = credentials.filter(
     (credential) => credential.service.toLowerCase() !== service.toLowerCase()
   );
   const newDB: DB = {
-    credentials: [...filteredCredential, enCript(credential, key)],
+    credentials: [...filteredCredential, encryptCredential(credential, key)],
   };
   await overWriteDB(newDB);
 }
@@ -59,8 +54,11 @@ export async function addCredential(
   credential: Credential,
   key: string
 ): Promise<void> {
-  const response = await readFile('src/db.json', 'utf-8');
-  const db: DB = JSON.parse(response);
-  db.credentials = [...db.credentials, enCript(credential, key)];
-  overWriteDB(db);
+  const credentials = await readCredentials();
+
+  const db: DB = {
+    credentials: [...credentials, encryptCredential(credential, key)],
+  };
+
+  await overWriteDB(db);
 }
