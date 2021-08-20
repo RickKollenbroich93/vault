25f7b20a8c1ae649ca96e693e5e57e11f41855df
RickKollenbroich93 <rick.kollenbroich93@googlemail.com>
2021-08-20 17:38:08 +0200

    Create addPage

--- src/app/pages/AddPage/AddPage.tsx
+++ src/app/pages/AddPage/AddPage.tsx
@@ -1,19 +1,63 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './AddPage.module.css';
 
 export default function AddPage(): JSX.Element {
+  const [masterPassword, setMasterPassword] = useState('');
+  const [newService, setNewService] = useState('');
+  const [newUsername, setNewUsername] = useState('');
+  const [newPassword, setNewPassword] = useState('');
+
+  async function addCredential() {
+    const newCredential = {
+      service: newService,
+      username: newUsername,
+      password: newPassword,
+    };
+
+    const response = await fetch('/api/credentials/', {
+      method: 'POST',
+      headers: {
+        Authorization: masterPassword,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newCredential),
+    });
+    console.log(await response.json());
+  }
+
   return (
     <main className={styles.container}>
       <h1>ADD new Credential</h1>
-      <form>
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          addCredential();
+        }}
+      >
         <h4>Enter Service</h4>
-        <input type="text" placeholder="Service" />
+        <input
+          type="text"
+          placeholder="Service"
+          onChange={(event) => setNewService(event.target.value)}
+        />
         <h4>Enter Username</h4>
-        <input type="text" placeholder="Username" />
+        <input
+          type="text"
+          placeholder="Username"
+          onChange={(event) => setNewUsername(event.target.value)}
+        />
         <h4>Enter Password</h4>
-        <input type="text" placeholder="Password" />
+        <input
+          type="text"
+          placeholder="Password"
+          onChange={(event) => setNewPassword(event.target.value)}
+        />
         <h4>Enter MasterPassword</h4>
-        <input type="password" placeholder="MasterPassword" />
+        <input
+          type="password"
+          placeholder="MasterPassword"
+          onChange={(event) => setMasterPassword(event.target.value)}
+        />
 
         <button type="submit" className={styles.subBtn}>
           Submit
