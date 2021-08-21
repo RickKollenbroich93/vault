1a7188dd48bb9f429f373cbae529008299447fbf
RickKollenbroich93 <rick.kollenbroich93@googlemail.com>
2021-08-21 15:17:46 +0200

    Add Edit function

--- src/app/pages/EditPage/EditPage.tsx
+++ src/app/pages/EditPage/EditPage.tsx
@@ -6,28 +6,47 @@ export default function AddPage(): JSX.Element {
   const [newService, setNewService] = useState('');
   const [newUsername, setNewUsername] = useState('');
   const [newPassword, setNewPassword] = useState('');
+
+  async function addCredential() {
+    const newCredential = {
+      service: newService,
+      username: newUsername,
+      password: newPassword,
+    };
+
+    const response = await fetch(`/api/credentials/${newCredential.service}`, {
+      method: 'PATCH',
+      headers: {
+        Authorization: masterPassword,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newCredential),
+    });
+    console.log(await response.json());
+  }
+
   return (
     <main className={styles.container}>
       <h1>Edit your Credential</h1>
       <form
-      // onSubmit={(event) => {
-      //   event.preventDefault();
-      //   addCredential();
-      // }}
+        onSubmit={(event) => {
+          event.preventDefault();
+          addCredential();
+        }}
       >
-        <h4>Enter Service</h4>
+        <h4>Enter Service you want to Edit</h4>
         <input
           type="text"
           placeholder="Service"
           onChange={(event) => setNewService(event.target.value)}
         />
-        <h4>Enter Username</h4>
+        <h4>Enter new Username</h4>
         <input
           type="text"
           placeholder="Username"
           onChange={(event) => setNewUsername(event.target.value)}
         />
-        <h4>Enter Password</h4>
+        <h4>Enter new Password</h4>
         <input
           type="text"
           placeholder="Password"
