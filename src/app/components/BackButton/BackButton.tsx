6cb0ea1f59998c92702a1e5020141cf4680840ab
RickKollenbroich93 <rick.kollenbroich93@googlemail.com>
2021-08-23 10:40:03 +0200

    Fix issue with buttons on addpage

--- src/app/components/BackButton/BackButton.tsx
+++ src/app/components/BackButton/BackButton.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from './BackButton.module.css';
 import { Link } from 'react-router-dom';
 
-export default function Forgot(): JSX.Element {
+export default function BackButton(): JSX.Element {
   return (
     <Link to="/">
       <button className={styles.backBtn}>🔙</button>
--- src/app/pages/AddPage/AddPage.tsx
+++ src/app/pages/AddPage/AddPage.tsx
@@ -30,47 +30,56 @@ export default function AddPage(): JSX.Element {
   return (
     <main className={styles.container}>
       <GlitchHeader />
-      <form
-        onSubmit={(event) => {
-          event.preventDefault();
-          addCredential();
-        }}
-      >
-        <div className={styles.contentWrapper}>
+      <div className={styles.pageWrapper}>
+        <BackButton />
+        <form
+          onSubmit={(event) => {
+            event.preventDefault();
+            addCredential();
+          }}
+        >
           <h4>Enter Service</h4>
           <input
             type="text"
             placeholder="Service"
+            value={newService}
             onChange={(event) => setNewService(event.target.value)}
+            required
           />
           <h4>Enter Username</h4>
           <input
             type="text"
             placeholder="Username"
+            value={newUsername}
             onChange={(event) => setNewUsername(event.target.value)}
+            required
           />
           <h4>Enter Password</h4>
           <input
             type="text"
             placeholder="Password"
+            value={newPassword}
             onChange={(event) => setNewPassword(event.target.value)}
+            required
           />
 
           <h4>Enter MasterPassword</h4>
-        </div>
-        <div className={styles.actionWrapper}>
-          <BackButton />
-          <input
-            type="password"
-            placeholder="MasterPassword"
-            onChange={(event) => setMasterPassword(event.target.value)}
-          />
 
-          <button type="submit" className={styles.subBtn}>
-            Submit
-          </button>
-        </div>
-      </form>
+          <div className={styles.actionWrapper}>
+            <input
+              type="password"
+              placeholder="MasterPassword"
+              value={masterPassword}
+              onChange={(event) => setMasterPassword(event.target.value)}
+              required
+            />
+
+            <button type="submit" className={styles.subBtn}>
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
     </main>
   );
 }
